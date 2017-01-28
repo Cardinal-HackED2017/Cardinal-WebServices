41fb84f8da1be64fe9889671b1859279b9833610
Ryan Huard <ryan.huard@live.ca>
2017-01-28 14:18:07 -0700

    Sql part 2: The Sequel

--- Scripts/createTables.sql
+++ Scripts/createTables.sql
@@ -12,18 +12,19 @@ CREATE TABLE Meetings(
 );
 
 CREATE TABLE Meeting_Times (
-    start_time timestamp,
+    start_time timestamp
     ,meeting_id uuid
     ,PRIMARY KEY(start_time, meeting_id)
     ,FOREIGN KEY (meeting_id) REFERENCES Meetings ON DELETE CASCADE
-)
+);
 
 CREATE TABLE Users (
     user_id uuid
     ,display_name text
     ,email text
     ,PRIMARY KEY (user_id)
-)
+);
+
 CREATE TABLE Messages (
     message_id uuid
     ,meeting_id uuid
@@ -33,7 +34,7 @@ CREATE TABLE Messages (
     ,PRIMARY KEY(message_id, meeting_id, user_id)
     ,FOREIGN KEY (meeting_id) REFERENCES Meetings
     ,FOREIGN KEY (user_id) REFERENCES Users
-)
+);
 
 CREATE TABLE Attendees (
     meeting_id uuid
@@ -41,4 +42,4 @@ CREATE TABLE Attendees (
     ,PRIMARY KEY (meeting_id, user_id)
     ,FOREIGN KEY (meeting_id) REFERENCES Meetings
     ,FOREIGN KEY (user_id) REFERENCES Users
-)
+);
