15f241ea0c5601c7b09150a0307131e511bdf461
George Antonious <george@antonious.ca>
2017-01-28 16:25:38 -0700

    Define username file

--- DataModels/User.cs
+++ DataModels/User.cs
@@ -1,7 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace cardinal_webservices.DataModels 
 {
     public class User 
     {
-
+        [Key, Column("user_id")]
+        public Guid Id { get; set; }
+        [Column("display_name")]
+        public string DisplayName { get; set; }
+        [Column("email")]
+        public string Email { get; set; }
     }
 }
