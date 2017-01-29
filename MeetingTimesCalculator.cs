0e1ab0a9e08b1fff954c33ddbffbfe816829fbc8
George Antonious <george@antonious.ca>
2017-01-28 23:26:22 -0700

    Stub meeting times calculator and notifer services

---
+++ MeetingTimesCalculator.cs
@@ -0,0 +1,10 @@
+namespace cardinal_webservices 
+{
+    public class MeetingTimesCalculator 
+    {
+        public async void ProcessUserJoinMeeting(string userId, string userCalendarToken) 
+        {
+            
+        }   
+    }
+}
