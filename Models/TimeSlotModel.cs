81777b901799d621e4d60219757aae0956520a47
Ryan Huard <ryan.huard@live.ca>
2017-01-29 05:44:04 -0700

    Did you know that date math is saddening?

--- Models/TimeSlotModel.cs
+++ Models/TimeSlotModel.cs
@@ -4,6 +4,14 @@ namespace cardinal_webservices.Models
 {
     public class TimeSlotModel
     {
+        public enum intersections
+        {
+            No_Intersect,
+            A_Before_B,
+            B_Before_A,
+            A_inside_B,
+            B_inside_A
+        }
         public DateTime start {get; set;}
         public TimeSpan length {get; set;}
 
@@ -11,5 +19,45 @@ namespace cardinal_webservices.Models
         {
             return start.Add(length);
         }
+
+        public string ToString()
+        {
+            return this.start.ToString() + this.length.ToString();
+        }
+
+        public static intersections intersects(TimeSlotModel a, TimeSlotModel b)
+        {
+            if(a.start == a.end() || b.start == b.end())
+            {
+                return intersections.No_Intersect;
+            }
+
+            if (a.start <= b.start)
+            {
+                if (a.end() >= b.start && a.end() <= b.end())
+                {
+                    return intersections.A_Before_B;
+                }
+                if (a.end() >= b.end())
+                {
+                    return intersections.B_inside_A;
+                }
+            }
+            else
+            {
+                if (b.end() >= a.start && b.end() <= a.end())
+                {
+                    return intersections.B_Before_A;
+                }
+                if (b.end() >= a.end())
+                {
+                    return intersections.A_inside_B;
+                }
+            }
+            return intersections.No_Intersect;
+        }
     }
+
+    public class InvalidTimeSlotException : Exception
+    {}
 }
