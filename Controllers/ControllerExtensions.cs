8073ccc38b897c7c76c446aed1fb0e420898d066
George Antonious <george@antonious.ca>
2017-01-28 23:08:36 -0700

    Extract user Id from request headers

--- Controllers/ControllerExtensions.cs
+++ Controllers/ControllerExtensions.cs
@@ -5,16 +5,16 @@ namespace cardinal_webservices.Controllers
 {
     public static class ControllerExtensions
     {
-        public static User GetCallingUser(this Controller controller) 
+        public static string GetCallingUserId(this Controller controller) 
         {
-            string NUUID = "6f2241ae-da64-4aa8-a414-308d8f900057";
-            
-            return new User 
+            try 
             {
-                Id = NUUID,
-                DisplayName = "Null",
-                Email = "null@null.nil"
-            };
+                return controller.HttpContext.Request.Headers["e-mail"];
+            }
+            catch
+            {
+                return "6f2241ae-da64-4aa8-a414-308d8f900057";
+            }
         }
     }
 }
