f084a4f4034061dcb16bfe89e734b999364b1cbb
Ryan Huard <ryan.huard@live.ca>
2017-01-28 17:31:04 -0700

    Meeting participation data model

--- DataModels/MeetingParticipation.cs
+++ DataModels/MeetingParticipation.cs
@@ -1,7 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace cardinal_webservices.DataModels 
 {
     public class MeetingParticipation
     {
-
+        [Key, Column("meeting_id")]
+        public string MeetingId { get; set; }
+        [Key, Column("user_id")]
+        public string UserId { get; set; }
     }
 }
