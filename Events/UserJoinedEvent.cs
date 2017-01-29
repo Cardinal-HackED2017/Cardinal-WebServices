6a9f8b1b283b5aaf5ea72a3b4e61221529bf4dab
Ryan Huard <ryan.huard@live.ca>
2017-01-29 01:01:16 -0700

    Implement user joined event

--- Events/UserJoinedEvent.cs
+++ Events/UserJoinedEvent.cs
@@ -1,7 +1,21 @@
+using cardinal_webservices.DataModels;
+using Newtonsoft.Json;
+
 namespace cardinal_webservices.Events 
 {
     public class UserJoinedEvent
     {
-        
+        [JsonProperty("type")]
+        public string Type {get; set;}
+        [JsonProperty("payload")]
+        public object Payload {get; set;}
+
+        public static UserJoinedEvent FromUser(User user)
+        {
+            return new UserJoinedEvent
+            {
+                Payload = user
+            };
+        }
     }
 }
