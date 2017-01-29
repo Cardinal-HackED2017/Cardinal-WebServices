af8e3e5c09cdc41244b5518ccdedb28c3d0d40a2
George Antonious <george@antonious.ca>
2017-01-28 17:23:00 -0700

    Update schema to use text instead of uuid for id types

--- Scripts/createTables.sql
+++ Scripts/createTables.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS Messages CASCADE;
 DROP TABLE IF EXISTS Attendees CASCADE;
 
 CREATE TABLE Meetings(
-    meeting_id uuid
+    meeting_id text
     ,name text
     ,create_time timestamp
     ,start_fence timestamp
@@ -18,24 +18,24 @@ CREATE TABLE Meetings(
 );
 
 CREATE TABLE Meeting_Times (
-    meeting_time_id uuid
+    meeting_time_id text
     ,start_time timestamp
-    ,meeting_id uuid
+    ,meeting_id text
     ,PRIMARY KEY(start_time, meeting_id)
     ,FOREIGN KEY (meeting_id) REFERENCES Meetings ON DELETE CASCADE
 );
 
 CREATE TABLE Users (
-    user_id uuid
+    user_id text
     ,display_name text
     ,email text
     ,PRIMARY KEY (user_id)
 );
 
 CREATE TABLE Messages (
-    message_id uuid
-    ,meeting_id uuid
-    ,user_id uuid
+    message_id text
+    ,meeting_id text
+    ,user_id text
     ,content text
     ,created_time timestamp
     ,PRIMARY KEY(message_id, meeting_id, user_id)
@@ -44,8 +44,8 @@ CREATE TABLE Messages (
 );
 
 CREATE TABLE Attendees (
-    meeting_id uuid
-    ,user_id uuid
+    meeting_id text
+    ,user_id text
     ,PRIMARY KEY (meeting_id, user_id)
     ,FOREIGN KEY (meeting_id) REFERENCES Meetings
     ,FOREIGN KEY (user_id) REFERENCES Users
