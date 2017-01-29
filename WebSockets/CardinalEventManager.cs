0927aac2d6fd0c21bb53518e1fabac06a7651bf6
George Antonious <george@antonious.ca>
2017-01-28 23:55:49 -0700

    Provide method to broadcast message created event

--- WebSockets/CardinalEventManager.cs
+++ WebSockets/CardinalEventManager.cs
@@ -1,22 +1,37 @@
 using System.Collections.Generic;
 using System.Net.WebSockets;
+using System.Linq;
+using System.Threading.Tasks;
 using cardinal_webservices.Data;
+using cardinal_webservices.DataModels;
+using cardinal_webservices.Events;
 
 namespace cardinal_webservices.WebSockets 
 {
     public class CardinalEventManager 
     {
         private readonly IEnumerable<CardinalWebSocket> _sockets;
-        private readonly ICardinalDataService cardinalDataService;
+        private readonly ICardinalDataService _cardinalDataService;
 
         public CardinalEventManager(ICardinalDataService cardinalDataService) 
         {
             _sockets = new List<CardinalWebSocket>();
+            _cardinalDataService = cardinalDataService;   
+        }
+        
+        public async void OnMessageCreated(Message message) 
+        {
+            var sendTasks = _sockets.Where(s => IsUserInMeeting(s.UserId, message.MeetingId))
+                                    .Select(s => s.SendObjectAsync(MessageCreatedEvent.FromMessage(message)));
+
+            await Task.WhenAll(sendTasks);
         }
 
-        public void OnMessageCreated() 
+        private bool IsUserInMeeting(string userId, string meetingId) 
         {
-
+            return _cardinalDataService.GetMeetingParticipations()
+                                       .Where(p => p.UserId == userId && p.MeetingId == meetingId)
+                                       .Any();
         }
     }
 }
