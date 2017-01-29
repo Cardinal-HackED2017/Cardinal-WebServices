7bc7630a74c5a1e22c45ee1cdf293e8c64eaec05
Ryan Huard <ryan.huard@live.ca>
2017-01-28 17:31:20 -0700

    Message data model

--- DataModels/Message.cs
+++ DataModels/Message.cs
@@ -1,7 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace cardinal_webservices.DataModels 
 {
     public class Message 
     {
-
+        [Key, Column("message_id")]
+        public string MessageId { get; set; }
+        [Key, Column("meeting_id")]
+        public string MeetingId { get; set; }
+        [Key, Column("user_id")]
+        public string UserId { get; set; }
+        [Column("content")]
+        public string content {get; set; }
+        [Column("created_time")]
+        public DateTime CreatedTime;
     }
 }
